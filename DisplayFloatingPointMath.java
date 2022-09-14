5220af7ffb806aec31fa45901e8e14152d44e5eb
Joana Morais <92741380+jalvesm@users.noreply.github.com>
2022-09-14 14:52:10 -0300

    calculate & display the pointing float num

---
+++ DisplayFloatingPointMath.java
@@ -0,0 +1,11 @@
+// Floating point Math: https://0.30000000000000004.com/ 
+
+public class DisplayFloatingPointMath {
+	public static void main(String[] args) {
+		
+		double firstValue = 0.1;
+		double secondValue = 0.2;
+		double sumBetweenThem = firstValue + secondValue; 
+		System.out.println(sumBetweenThem);
+	}
+}
