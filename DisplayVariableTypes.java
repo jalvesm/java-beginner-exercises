52284c43b55625473322ca326baedc5b3621879e
Joana Morais <92741380+jalvesm@users.noreply.github.com>
2022-09-14 15:03:28 -0300

    Stores and displays different data types.
    This algorithm stores and displays different values to the different data types.

---
+++ DisplayVariableTypes.java
@@ -0,0 +1,33 @@
+
+public class DisplayVariableTypes {
+	public static void main(final String[] args) {
+
+		System.out.println("Variables in Java:\n");
+		final int myNumber = 88;
+		System.out.println("\tThe 'int' type: " + myNumber);
+
+		final short myShort = 847;
+		System.out.println("\tThe 'short' type: " + myShort);
+
+		final long myLong = 9797;
+		System.out.println("\tThe 'long' type: " + myLong);
+
+		final double myDouble = 7.3243;
+		System.out.println("\tThe 'double' type: " + myDouble);
+
+		final float myFloat = 3.14f; // pi
+		System.out.println("\tThe 'float' type: " + myFloat);	         
+
+		final char myChar = 'y';
+		System.out.println("\tThe 'char' type: " + myChar);
+
+		final boolean myBoolean = false;
+		System.out.println("\tThe 'boolean' type: " + myBoolean);
+
+		final byte myByte = 127;
+		System.out.println("\tThe 'byte' type: " + myByte);
+		
+		System.out.println("\nEnd of program!");
+
+	}
+}
